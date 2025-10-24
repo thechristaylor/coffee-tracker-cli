917e884df0d61a332bacf94e90cf533ae6701828
ctaylor20 <ctaylor20@qub.ac.uk>
2025-10-24 21:20:04 +0100

    add new flagsets for week, month, year & all. Standardised field names in CoffeeLog and data.json for better data unmarshaling

--- main.go
+++ main.go
@@ -1,38 +1,42 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"log"
 	"os"
 	"time"
 )
 
 type CoffeeLog struct {
-	Id         int
-	Timestamp  int64
+	ID         int
+	Timestamp  string
 	CoffeeType string
 	Venue      string
 	Size       string
 }
 
 var file = "data.json"
+var seedData []CoffeeLog
 
+// If the read or unmarshalling fails the program should end
 func init() {
-	bs, err := os.ReadFile("data.json")
+	bs, err := os.ReadFile("/Users/chris/Projects/go/coffee/data.json")
 	if err != nil {
-		fmt.Println("error reading data.json file %w", err.Error())
+		log.Fatal(err)
 	}
-	seedData := string(bs)
-	fmt.Printf("seedData: %v", seedData)
+	unmarshalErr := json.Unmarshal(bs, &seedData)
+	if unmarshalErr != nil {
+		log.Fatal(unmarshalErr)
+	}
 }
 
-func lastId() (int, error) {
-	data, err := os.ReadFile(file)
-	if err != nil {
-		fmt.Println("error reading data.json file: %w", err.Error())
-	}
-	fmt.Printf("data contains: %v", data)
-	return 1, nil
+// get the len of seedData to find the lastId
+func lastId(seedData []CoffeeLog) (int, error) {
+	lastId := len(seedData) - 1
+	fmt.Printf("lastId is: %v", lastId)
+	return lastId, nil
 }
 
 func (c CoffeeLog) addCoffee() (string, error) {
@@ -46,7 +50,12 @@ func (c CoffeeLog) addCoffee() (string, error) {
 
 func main() {
 
-	cmd := os.Args[1]
+	cmd := ""
+
+	if len(os.Args) > 1 {
+		cmd = os.Args[1]
+	}
+
 	fmt.Println("os.Args[1]: ", cmd)
 
 	if cmd == "" {
@@ -69,18 +78,78 @@ func main() {
 		addFlags.Parse(flags)
 	}
 
-	if cmd == "stats" {
-		// list current status
+	if cmd == "today" {
+		// list todays coffee consumption stats
 		flags := os.Args[2:]
-		day := time.Now().Day()
-		fmt.Println("Lets look at the tally... ")
-		fmt.Printf("flags contains:  %v", flags)
-		statsFlags := flag.NewFlagSet("stats", flag.ExitOnError)
-		statsFlags.Duration("day", time.Duration(day), "The day that you want to get stats for")
-		// add a week range
-		statsFlags.Duration("week", time.Duration(day), "The week you want to get stats for") // need to fix this flag
-		statsFlags.Duration("month", time.Duration(time.Now().Month()), "The month that you want to get stats for")
-		// add a custom range flag
-		statsFlags.Duration("custom", time.Duration(day), "the custom date range you want stats for")
+		year, month, day := time.Now().Date()
+		todayFlags := flag.NewFlagSet("today", flag.ExitOnError)
+		todayFlags.String("type", "americano", "filter by type (optional)")
+		todayFlags.String("t", "americano", "filter by type (optional)")
+		todayFlags.String("venue", "home", "filter by venue (optional)")
+		todayFlags.String("v", "home", "filter by venue (optional)")
+		todayFlags.String("size", "medium", "filter by size (optional)")
+		todayFlags.String("s", "medium", "filter by size (optional)")
+
+		todayFlags.Parse(flags)
+	}
+
+	if cmd == "week" {
+		// list coffee consumption for this week
+		flags := os.Args[2:]
+		year, month, day := time.Now().Date()
+		weekFlags := flag.NewFlagSet("today", flag.ExitOnError)
+		weekFlags.String("type", "americano", "filter by type (optional)")
+		weekFlags.String("t", "americano", "filter by type (optional)")
+		weekFlags.String("venue", "home", "filter by venue (optional)")
+		weekFlags.String("v", "home", "filter by venue (optional)")
+		weekFlags.String("size", "medium", "filter by size (optional)")
+		weekFlags.String("s", "medium", "filter by size (optional)")
+
+		weekFlags.Parse(flags)
+	}
+
+	if cmd == "month" {
+		// list coffee consumption for this month
+		flags := os.Args[2:]
+		year, month, day := time.Now().Date()
+		monthFlags := flag.NewFlagSet("today", flag.ExitOnError)
+		monthFlags.String("type", "americano", "filter by type (optional)")
+		monthFlags.String("t", "americano", "filter by type (optional)")
+		monthFlags.String("venue", "home", "filter by venue (optional)")
+		monthFlags.String("v", "home", "filter by venue (optional)")
+		monthFlags.String("size", "medium", "filter by size (optional)")
+		monthFlags.String("s", "medium", "filter by size (optional)")
+
+		monthFlags.Parse(flags)
+	}
+
+	if cmd == "year" {
+		// list coffee consumption for this year
+		flags := os.Args[2:]
+		year, month, day := time.Now().Date()
+		yearFlags := flag.NewFlagSet("today", flag.ExitOnError)
+		yearFlags.String("type", "americano", "filter by type (optional)")
+		yearFlags.String("t", "americano", "filter by type (optional)")
+		yearFlags.String("venue", "home", "filter by venue (optional)")
+		yearFlags.String("v", "home", "filter by venue (optional)")
+		yearFlags.String("size", "medium", "filter by size (optional)")
+		yearFlags.String("s", "medium", "filter by size (optional)")
+
+		yearFlags.Parse(flags)
+	}
+
+	if cmd == "all" {
+		// list coffee consumption for this all time.
+		flags := os.Args[2:]
+		year, month, day := time.Now().Date()
+		allTimeFlags := flag.NewFlagSet("today", flag.ExitOnError)
+		allTimeFlags.String("type", "americano", "filter by type (optional)")
+		allTimeFlags.String("t", "americano", "filter by type (optional)")
+		allTimeFlags.String("venue", "home", "filter by venue (optional)")
+		allTimeFlags.String("v", "home", "filter by venue (optional)")
+		allTimeFlags.String("size", "medium", "filter by size (optional)")
+		allTimeFlags.String("s", "medium", "filter by size (optional)")
+
+		allTimeFlags.Parse(flags)
 	}
 }
